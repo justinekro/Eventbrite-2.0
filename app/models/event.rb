45978ccb607a36f303be0fb8dc987684b60a150d
Justine <>
2018-02-08 17:39:39 +0100

    tried to delete bug

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,5 +12,5 @@ class Event < ApplicationRecord
 #  	def date_cannot_be_in_the_past
 #    errors.add(:date, "can't be in the past") if
 #      !date.blank? and date < Date.today
-  	end
+#  	end
 end
